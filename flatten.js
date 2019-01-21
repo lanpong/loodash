98162f0d9772d13af5c14ffdd3b34045e12b428a
lanpong <hdy95@outlook.com>
2019-01-21 19:13:25 +0800

    flatten

---
+++ flatten.js
@@ -0,0 +1,12 @@
+/**
+ * 减少一级array嵌套深度
+ * @param {Array} array 
+ * @returns {Array}
+ * @example
+ * flatten([1, [2, [3, [4]], 5]])
+ * // => [1, 2 ,[3, [4]], 5]
+ */
+function flatten (array) {
+    let length = array == null ? 0 : array.length;
+
+}
