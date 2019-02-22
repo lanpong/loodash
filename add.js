e3d9d2f7030c775a6ea4deeced3da81e219e1cb6
lanpong <hdy95@outlook.com>
2019-02-22 21:29:37 +0800

    add

---
+++ add.js
@@ -0,0 +1,12 @@
+/**
+ * 两个数相加
+ * @param {Number} augend 
+ * @param {Number} addend 
+ * @returns {Number}
+ * @example
+ * add(6, 4);
+ * // => 10
+ */
+function add(augend, addend) {
+    return augend + addend;
+}
