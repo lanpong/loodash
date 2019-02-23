552c9975294f8b4a61b8d185ed32b2e2aa90c90c
lanpong <hdy95@outlook.com>
2019-02-23 10:55:17 +0800

    divide

---
+++ divide.js
@@ -0,0 +1,12 @@
+/**
+ * 两个数相除
+ * @param {Number} dividend 
+ * @param {Numver} divisor 
+ * @returns {Number}
+ * @example
+ * divide(6, 4)
+ * // => 1.5
+ */
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}
