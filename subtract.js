74fcfd0430bc60a30dcd1d12c426e458578842ef
lanpong <hdy95@outlook.com>
2019-02-24 19:32:22 +0800

    subtract

---
+++ subtract.js
@@ -0,0 +1,12 @@
+/**
+ * 两数相减
+ * @param {number} minuend 
+ * @param {number} subtrahend 
+ * @returns {number}
+ * @example
+ * subtract(6, 4)
+ * // => 2
+ */
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
