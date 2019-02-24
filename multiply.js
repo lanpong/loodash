8baa81447476dd5830a08184300b40df729ea8f0
lanpong <hdy95@outlook.com>
2019-02-24 19:31:39 +0800

    multiply

---
+++ multiply.js
@@ -0,0 +1,12 @@
+/**
+ * 两个数相乘
+ * @param {number} multiplier 
+ * @param {number} multiplicand 
+ * @returns {number}
+ * @example
+ * multiply(6, 4)
+ * // => 24
+ */
+function multiply(multiplier, multiplicand) {
+    return multiplier * multiplicand;
+}
