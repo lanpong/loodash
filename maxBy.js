d4f9f3ec79533c8dc3046f08a4bae5b5747263f9
lanpong <hdy95@outlook.com>
2019-02-26 19:55:33 +0800

    maxBy

---
+++ maxBy.js
@@ -0,0 +1,17 @@
+/**
+ * 这个方法类似 .max 除了它接受 iteratee 来调用 array中的每一个元素，
+ * 来生成其值排序的标准。 iteratee 会调用1个参数: (value)
+ * @param {Array} array 
+ * @param {Function} [iteratee=_.identity] 
+ * @returns {*}
+ * @example
+ * var objects = [{ 'n': 1 }, { 'n': 2 }]
+ * maxBy(objects, function(o) { return o.n; })
+ * // => { 'n': 2 }
+ * 
+ * maxBy(objects, 'n')
+ * // => { 'n': 2 }
+ */
+function maxBy(array, iteratee) {
+
+}
