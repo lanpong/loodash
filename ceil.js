66c3f70de14325e68e190e7a227b63f5bc8522d8
lanpong <hdy95@outlook.com>
2019-02-28 20:42:15 +0800

    ceil

--- ceil.js
+++ ceil.js
@@ -1,13 +1,15 @@
 /**
  * 根据 precision（精度） 向上舍入 number
- * @param {number} number 
- * @param {number} [precision=0] 
- * @returns {number}
+ * @param {number} number 要向上舍入的值
+ * @param {number} [precision=0] 向上舍入的的精度
+ * @returns {number} 返回向上舍入的值
  * @example
  * ceil(4.006)
  * // => 5
+ * 
  * ceil(6.004, 2)
  * // => 6.01
+ * 
  * ceil(6040, -2)
  * // => 6100
  */
