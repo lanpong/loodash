d0b3c25d9304be6a53f0c7a611ba3b9ea1bd2c6d
lanpong <hdy95@outlook.com>
2019-03-03 10:38:07 +0800

    matches

--- matches.js
+++ matches.js
@@ -1,3 +1,5 @@
+import isMatch from './isMatch.js'
+
 /**
  * 创建一个深比较的方法来比较给定的对象和 source 对象。
  * 如果给定的对象拥有相同的属性值返回 true，否则返回 false
@@ -5,12 +7,7 @@
  * @returns {Function} 返回新的函数
  */
 function matches(source) {
-    return function (obj) {
-        for (let key in source) {
-            if (obj[key] !== source[key]) {
-                return false;
-            }
-        }
-        return true;
-    }
+    return obj => isMatch(obj, source);
 }
+
+export default matches;
